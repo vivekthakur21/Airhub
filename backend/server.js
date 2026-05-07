b198bbdff959c465af87148dfaf7926258e98402
vivek thakur <vivthakur21@gmail.com>
2026-05-07 15:00:28 +0530

    fix: use regex literal for express 5 catch-all

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
 // All other GET requests not handled before will return our React app
-app.get('(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
 });
 
